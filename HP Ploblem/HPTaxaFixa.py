ea5053cf587feef59f45de2e3a845c7052b123c9
Bruna Torres <blts@ic.ufal.br>
2023-03-28 15:06:31 -0300

    HpFixo

--- HP Ploblem/HPTaxaFixa.py
+++ HP Ploblem/HPTaxaFixa.py
@@ -1,85 +1,75 @@
 import random
 
-# Define o tamanho da população e o número de gerações
+# Tamanho do cromossomo (proteína) e tamanho da população
+CHROMOSOME_SIZE = 20
 POPULATION_SIZE = 50
-GENERATIONS = 100
 
-# Define a taxa de mutação e cruzamento
+# Taxa de mutação e cruzamento
 MUTATION_RATE = 0.1
 CROSSOVER_RATE = 0.8
 
-# Define o tamanho da sequência de aminoácidos
-SEQUENCE_SIZE = 20
-
-# Define a sequência de aminoácidos
-SEQUENCE = "HPHPHPHPPHH"
-
-# Define a função de fitness
-def fitness_function(sequence):
-    # Cálculo da pontuação de fitness
+# Função de fitness
+def fitness(chromosome):
     score = 0
-    for i in range(len(sequence)-1):
-        for j in range(i+1,len(sequence)):
-            if abs(i-j) > 2:
-                score += 1 if sequence[i]=="H" and sequence[j]=="H" else 0
-                score += 1 if sequence[i]=="P" and sequence[j]=="P" else 0
-                score += 1 if sequence[i]=="H" and sequence[j]=="P" else 0
+    for i in range(CHROMOSOME_SIZE - 1):
+        if chromosome[i] == 'H' and chromosome[i + 1] == 'P':
+            score += 1
+        elif chromosome[i] == 'P' and chromosome[i + 1] == 'H':
+            score += 1
     return score
 
-# Define a função para criar a população inicial
-def create_population(size):
+# Geração da população inicial
+def generate_population():
     population = []
-    for i in range(size):
-        sequence = ''.join(random.sample(SEQUENCE, SEQUENCE_SIZE))
-        population.append(sequence)
+    for i in range(POPULATION_SIZE):
+        chromosome = ''.join(random.choices(['H', 'P'], k=CHROMOSOME_SIZE))
+        population.append(chromosome)
     return population
 
-# Define a função de seleção de pais por torneio
+# Seleção dos pais utilizando torneio
 def selection(population):
     parents = []
     for i in range(2):
         tournament = random.sample(population, 5)
-        winner = max(tournament, key=lambda x: fitness_function(x))
+        winner = max(tournament, key=fitness)
         parents.append(winner)
     return parents
 
-# Define a função de crossover
-def crossover(parents):
-    if random.random() < CROSSOVER_RATE:
-        index = random.randint(1, SEQUENCE_SIZE-1)
-        child1 = parents[0][:index] + parents[1][index:]
-        child2 = parents[1][:index] + parents[0][index:]
-        return child1, child2
-    else:
-        return parents
+# Cruzamento de dois pais utilizando one-point crossover
+def crossover(parent1, parent2):
+    if random.random() > CROSSOVER_RATE:
+        return parent1, parent2
+    crossover_point = random.randint(1, CHROMOSOME_SIZE - 1)
+    child1 = parent1[:crossover_point] + parent2[crossover_point:]
+    child2 = parent2[:crossover_point] + parent1[crossover_point:]
+    return child1, child2
 
-# Define a função de mutação
-def mutation(individual):
-    if random.random() < MUTATION_RATE:
-        index = random.randint(0, SEQUENCE_SIZE-1)
-        new_char = random.choice(SEQUENCE)
-        individual = individual[:index] + new_char + individual[index+1:]
-    return individual
+# Mutação de um indivíduo trocando um gene aleatório
+def mutate(individual):
+    if random.random() > MUTATION_RATE:
+        return individual
+    mutation_point = random.randint(0, CHROMOSOME_SIZE - 1)
+    mutated_gene = 'H' if individual[mutation_point] == 'P' else 'P'
+    mutated_individual = individual[:mutation_point] + mutated_gene + individual[mutation_point + 1:]
+    return mutated_individual
 
-# Cria a população inicial
-population = create_population(POPULATION_SIZE)
+# Algoritmo genético completo
+def genetic_algorithm():
+    population = generate_population()
+    for generation in range(100):
+        print(f'Generation {generation + 1}')
+        # Avaliação da população
+        scores = [fitness(individual) for individual in population]
+        best_individual = max(population, key=fitness)
+        print(f'Best individual: {best_individual}, score: {fitness(best_individual)}')
+        # Seleção de pais e cruzamento
+        new_population = []
+        for i in range(POPULATION_SIZE // 2):
+            parent1, parent2 = selection(population)
+            child1, child2 = crossover(parent1, parent2)
+            new_population.append(mutate(child1))
+            new_population.append(mutate(child2))
+        population = new_population
 
-# Executa o algoritmo genético por um número de gerações especificado
-for generation in range(GENERATIONS):
-    print("Generation", generation+1)
-    
-    # Avalia a população atual
-    population_fitness = [(individual, fitness_function(individual)) for individual in population]
-    population_fitness = sorted(population_fitness, key=lambda x: x[1], reverse=True)
-    best_individual, best_fitness = population_fitness[0]
-    print("Best individual:", best_individual, "Fitness:", best_fitness)
-    
-    # Seleciona os pais para a próxima geração
-    new_population = []
-    for i in range(POPULATION_SIZE//2):
-        parents = selection(population)
-        children = crossover(parents)
-        child1 = mutation(children[0])
-        child2 = mutation(children[1])
-        new_population.extend([child1, child2])
-    population = new_population
+# Execução do algoritmo
+genetic_algorithm()
